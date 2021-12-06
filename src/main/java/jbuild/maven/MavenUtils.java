7b2be64b32567ca6e00320f8ad2f18471ced84f7
Renato Athaydes <renato@athaydes.com>
2021-12-06 19:34:04 +0100

    Store XML DocumentFactory in singleton.

--- src/main/java/jbuild/maven/MavenUtils.java
+++ src/main/java/jbuild/maven/MavenUtils.java
@@ -4,7 +4,6 @@ import jbuild.artifact.Artifact;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
@@ -21,6 +20,22 @@ public final class MavenUtils {
 
     public static final String MAVEN_CENTRAL_URL = "https://repo1.maven.org/maven2";
 
+    private enum XmlSingletons {
+        INSTANCE;
+
+        private final DocumentBuilderFactory factory;
+
+        XmlSingletons() {
+            factory = DocumentBuilderFactory.newInstance();
+            try {
+                factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            } catch (ParserConfigurationException e) {
+                throw new RuntimeException("Cannot parse XML without feature: " +
+                        XMLConstants.FEATURE_SECURE_PROCESSING);
+            }
+        }
+    }
+
     public static String standardArtifactPath(Artifact artifact, boolean usePlatformSeparator) {
         var fileName = artifact.toFileName();
         var sep = usePlatformSeparator ? File.separatorChar : '/';
@@ -31,9 +46,7 @@ public final class MavenUtils {
     }
 
     public static MavenPom parsePom(InputStream stream) throws ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-        DocumentBuilder db = dbf.newDocumentBuilder();
+        var db = XmlSingletons.INSTANCE.factory.newDocumentBuilder();
         try (stream) {
             return new MavenPom(db.parse(stream));
         }
@@ -41,9 +54,7 @@ public final class MavenUtils {
 
     public static MavenMetadata parseMavenMetadata(InputStream stream)
             throws ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-        DocumentBuilder db = dbf.newDocumentBuilder();
+        var db = XmlSingletons.INSTANCE.factory.newDocumentBuilder();
         try (stream) {
             return new MavenMetadata(db.parse(stream));
         }
@@ -57,7 +68,7 @@ public final class MavenUtils {
 
     public static String resolveProperty(String value, Map<String, String> properties) {
         if (value.startsWith("${") && value.endsWith("}")) {
-            var key= value.substring(2, value.length() - 1);
+            var key = value.substring(2, value.length() - 1);
             return properties.getOrDefault(key, value);
         }
         return value;
