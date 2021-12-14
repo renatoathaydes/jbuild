7f6cf2e0df5d75002abab1d3fe02cd638a139a90
Renato Athaydes <renato@athaydes.com>
2021-12-14 19:15:33 +0100

    Fixed transitivity of Maven scopes.

--- src/main/java/jbuild/maven/Scope.java
+++ src/main/java/jbuild/maven/Scope.java
@@ -8,17 +8,23 @@ public enum Scope {
 
     ;
 
+    /**
+     * Check if this scope should include transitive dependencies with the given scope.
+     *
+     * @param other scope to check
+     * @return true if this scope transitively includes dependencies with the given scope,
+     * false otherwise
+     */
     boolean includes(Scope other) {
         switch (this) {
             case COMPILE:
+                return other == COMPILE;
+            case RUNTIME:
+            case TEST:
+                return other == COMPILE || other == RUNTIME;
             case PROVIDED:
             case SYSTEM:
             case IMPORT:
-                return this == other;
-            case RUNTIME:
-                return other == COMPILE || other == RUNTIME;
-            case TEST:
-                return other == COMPILE || other == RUNTIME || other == TEST;
             default:
                 return false;
         }
