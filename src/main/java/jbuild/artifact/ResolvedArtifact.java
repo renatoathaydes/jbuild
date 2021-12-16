3fb8b647ca2d18d78391adac14741e5d261288fa
Renato Athaydes <renato@athaydes.com>
2021-12-16 21:16:45 +0100

    Improved and documented ResolvedArtifact

--- src/main/java/jbuild/artifact/ResolvedArtifact.java
+++ src/main/java/jbuild/artifact/ResolvedArtifact.java
@@ -6,7 +6,21 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.time.Instant;
 
+/**
+ * Resolved artifact.
+ * <p>
+ * Unlike most other JBuild classes, {@link ResolvedArtifact} is not completely immutable for memory
+ * performance reasons. The contents of an artifact may be fairly large and keeping it around for longer
+ * than necessary would cause more memory than necessary to be held by the JVM for no reason. For this
+ * reason, it is advisable that the contents of a resolved artifact be <em>consumed</em> via one of the
+ * consume methods, which can be called only once as after that, attempting to access the contents of this
+ * artifact will result in an error.
+ * <p>
+ * To read the contents more than once, the {@link ResolvedArtifact#getContents()} getter may be used,
+ * but that's only advisable in case it is known that another artifact handler will later consume it.
+ */
 public final class ResolvedArtifact {
+
     private byte[] contents;
     public final long contentLength;
     public final Artifact artifact;
@@ -34,20 +48,42 @@ public final class ResolvedArtifact {
                 '}';
     }
 
-    public void consumeContents(OutputStream writer) throws IOException {
+    /**
+     * @return the resolved contents of this artifact.
+     */
+    public byte[] getContents() {
         var c = contents;
         if (c == null) {
             throw new IllegalStateException("artifact contents already consumed: " + artifact);
         }
+        return c;
+    }
+
+    /**
+     * Consume the contents of this artifact.
+     * <p>
+     * After this method is called, the contents of this artifact will be "forgotten" and attempting
+     * to read its contents again will result in an {@link IllegalStateException} being thrown.
+     *
+     * @param writer into which to write the contents of this artifact.
+     * @throws IOException if an error occurs while writing the artifact.
+     */
+    public void consumeContents(OutputStream writer) throws IOException {
+        var c = getContents();
         writer.write(c);
         contents = null;
     }
 
+    /**
+     * Consume the contents of this artifact.
+     * <p>
+     * After this method is called, the contents of this artifact will be "forgotten" and attempting
+     * to read its contents again will result in an {@link IllegalStateException} being thrown.
+     *
+     * @return an in-memory {@link InputStream} with the resolved contents of this artifact.
+     */
     public InputStream consumeContents() {
-        var c = contents;
-        if (c == null) {
-            throw new IllegalStateException("artifact contents already consumed: " + artifact);
-        }
+        var c = getContents();
         contents = null;
         return new ByteArrayInputStream(c);
     }
