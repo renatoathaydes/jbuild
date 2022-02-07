b73b1e5f04bbd64c96600b7317b0a1bd5f45ab91
Renato Athaydes <renato@athaydes.com>
2022-02-07 23:25:59 +0100

    Created Version class.
    Necessary for resolving Maven version ranges.

---
+++ src/main/java/jbuild/artifact/Version.java
@@ -0,0 +1,94 @@
+package jbuild.artifact;
+
+/**
+ * Semantic version.
+ */
+public final class Version implements Comparable<Version> {
+
+    public final int major;
+    public final int minor;
+    public final int patch;
+    public final String qualifier;
+
+    public Version(int major, int minor, int patch, String qualifier) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+        this.qualifier = qualifier;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Version version = (Version) o;
+
+        if (major != version.major) return false;
+        if (minor != version.minor) return false;
+        if (patch != version.patch) return false;
+        return qualifier.equals(version.qualifier);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = major;
+        result = 31 * result + minor;
+        result = 31 * result + patch;
+        result = 31 * result + qualifier.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return major + '.' + minor + '.' + patch + (qualifier.isBlank() ? "" : '-' + qualifier);
+    }
+
+    public static Version parse(String version) {
+        var parts = version.split("[+.\\-]", 4);
+        int major = 0, minor = 0, patch = 0;
+        if (parts.length == 0) {
+            return new Version(major, minor, patch, "");
+        }
+        try {
+            major = Integer.parseInt(parts[0]);
+        } catch (NumberFormatException e) {
+            return new Version(0, 0, 0, version);
+        }
+        if (parts.length > 1) try {
+            minor = Integer.parseInt(parts[1]);
+        } catch (NumberFormatException e) {
+            var qualifier = version.substring(parts[0].length() + 1);
+            return new Version(major, 0, 0, qualifier);
+        }
+        else {
+            return new Version(major, minor, patch, "");
+        }
+        if (parts.length > 2) try {
+            patch = Integer.parseInt(parts[2]);
+        } catch (NumberFormatException e) {
+            var qualifier = version.substring(parts[0].length() + parts[1].length() + 2);
+            return new Version(major, minor, 0, qualifier);
+        }
+        else {
+            return new Version(major, minor, patch, "");
+        }
+        var qualifier = parts.length > 3 ? parts[3] : "";
+        return new Version(major, minor, patch, qualifier);
+    }
+
+    @Override
+    public int compareTo(Version other) {
+        if (equals(other)) return 0;
+        if (major != other.major) {
+            return Integer.compare(major, other.major);
+        }
+        if (minor != other.minor) {
+            return Integer.compare(minor, other.minor);
+        }
+        if (patch != other.patch) {
+            return Integer.compare(patch, other.patch);
+        }
+        return qualifier.compareTo(other.qualifier);
+    }
+}
---
+++ src/test/java/jbuild/artifact/VersionTest.java
@@ -0,0 +1,56 @@
+package jbuild.artifact;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VersionTest {
+
+    static Stream<Object> versionExamples() {
+        return Stream.of(
+                new Object[]{"", new Version(0, 0, 0, "")},
+                new Object[]{"1", new Version(1, 0, 0, "")},
+                new Object[]{"1.1", new Version(1, 1, 0, "")},
+                new Object[]{"1.1.1", new Version(1, 1, 1, "")},
+                new Object[]{"1.2.3.4", new Version(1, 2, 3, "4")},
+                new Object[]{"foo", new Version(0, 0, 0, "foo")},
+                new Object[]{"10.foo.3", new Version(10, 0, 0, "foo.3")},
+                new Object[]{"1-2-3.alpha", new Version(1, 2, 3, "alpha")},
+                new Object[]{"4.5.6-beta-1", new Version(4, 5, 6, "beta-1")},
+                new Object[]{"4a.5.6-beta-1", new Version(0, 0, 0, "4a.5.6-beta-1")},
+                new Object[]{"1.0.0-alpha+001", new Version(1, 0, 0, "alpha+001")},
+                new Object[]{"1.0.0-0.3.7", new Version(1, 0, 0, "0.3.7")}
+        );
+    }
+
+    static Version[][] sortedVersionExamples() {
+        return new Version[][]{
+                {new Version(0, 0, 0, ""), new Version(1, 0, 0, ""), new Version(2, 0, 0, "")},
+                {new Version(0, 1, 0, ""), new Version(0, 2, 0, ""), new Version(0, 3, 0, "")},
+                {new Version(0, 0, 10, ""), new Version(0, 0, 20, ""), new Version(0, 0, 30, "")},
+                {new Version(0, 0, 0, "a"), new Version(0, 0, 0, "b"), new Version(0, 0, 0, "c")},
+                {new Version(10, 3, 0, ""), new Version(20, 2, 0, ""), new Version(30, 1, 0, "")},
+                {new Version(10, 0, 3, ""), new Version(20, 0, 2, ""), new Version(30, 0, 1, "")},
+                {new Version(10, 0, 0, "c"), new Version(20, 0, 0, "b"), new Version(30, 0, 0, "a")},
+                {new Version(10, 0, 0, "c"), new Version(20, 0, 0, "b"), new Version(30, 0, 0, "a")},
+        };
+    }
+
+    @MethodSource("versionExamples")
+    @ParameterizedTest
+    void canParseVersion(String version, Version expectedVersion) {
+        assertThat(Version.parse(version)).isEqualTo(expectedVersion);
+    }
+
+    @MethodSource("sortedVersionExamples")
+    @ParameterizedTest
+    void canSortVersions(Version v1, Version v2, Version v3) {
+        assertThat(v1).isLessThan(v2);
+        assertThat(v2).isLessThan(v3);
+        assertThat(v1).isLessThan(v3);
+    }
+
+}
