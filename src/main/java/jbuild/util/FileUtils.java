3546aff98613be908b7487eea417e552cf47d80e
Renato Athaydes <renato@athaydes.com>
2023-04-20 23:17:56 +0200

    Added support for patching jars.

--- src/main/java/jbuild/util/FileUtils.java
+++ src/main/java/jbuild/util/FileUtils.java
@@ -5,11 +5,14 @@ import jbuild.errors.JBuildException;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
 import java.nio.channels.CompletionHandler;
+import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -17,6 +20,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 import static java.util.stream.Collectors.toList;
 import static jbuild.errors.JBuildException.ErrorCause.USER_INPUT;
@@ -90,14 +96,14 @@ public final class FileUtils {
     }
 
     public static List<FileCollection> collectFiles(Set<String> directories,
-            FilenameFilter filter) {
+                                                    FilenameFilter filter) {
         return directories.stream()
                 .map(dirPath -> collectFiles(dirPath, filter))
                 .collect(toList());
     }
 
     public static FileCollection collectFiles(String dirPath,
-            FilenameFilter filter) {
+                                              FilenameFilter filter) {
         var dir = new File(dirPath);
         if (dir.isDirectory()) {
             var children = dir.listFiles();
@@ -108,6 +114,41 @@ public final class FileUtils {
             }
         }
         return new FileCollection(dirPath);
+    }
+
+    public static void patchJar(File jarFile,
+                                String baseDir,
+                                Set<String> add,
+                                Set<String> delete) throws IOException {
+        if (add.isEmpty() && delete.isEmpty()) return;
+        var newFile = Files.createTempFile("jbuild-temp-jar-", ".jar").toFile();
+        var newJar = new ZipOutputStream(new FileOutputStream(newFile));
+        try (var jar = new ZipFile(jarFile, ZipFile.OPEN_READ)) {
+            if (!delete.isEmpty()) {
+                var entries = jar.entries();
+                while (entries.hasMoreElements()) {
+                    var entry = entries.nextElement();
+                    if (delete.contains(entry.getName())) continue;
+                    newJar.putNextEntry(entry);
+                    var entryStream = jar.getInputStream(entry);
+                    entryStream.transferTo(newJar);
+                    entryStream.close();
+                    newJar.closeEntry();
+                }
+            }
+            for (var toAdd : add) {
+                var file = new File(baseDir, toAdd);
+                try (var in = new FileInputStream(file)) {
+                    newJar.putNextEntry(new ZipEntry(toAdd));
+                    in.transferTo(newJar);
+                    newJar.closeEntry();
+                }
+            }
+        }
+        newJar.close();
+        if (!newFile.renameTo(jarFile)) {
+            throw new IOException("unable to replace jar file with new contents: " + jarFile);
+        }
     }
 
     private static Stream<String> fileOrChildDirectories(File file, FilenameFilter filter) {
