3b5ca9ece9a39153857fad196065e718e65a4534
Renato Athaydes <renato@athaydes.com>
2023-11-10 22:11:45 +0100

    Adding jb-ext to generate version file.
    Bug in jb was found. Must fix first.

---
+++ jb-ext/src/jb/GenerateJBuildVersion.java
@@ -0,0 +1,36 @@
+package jb;
+
+import jbuild.api.JBuildException;
+import jbuild.api.JbTask;
+import jbuild.api.JbTaskInfo;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Files.writeString;
+import static jbuild.api.JBuildException.ErrorCause.IO_WRITE;
+
+@JbTaskInfo(name = "generateJBuildVersion",
+        description = "Generates a Version file so JBuild can report its own version.",
+        inputs = GenerateJBuildVersion.INPUT,
+        outputs = GenerateJBuildVersion.OUTPUT,
+        dependents = {"compile", "publicationCompile"})
+public class GenerateJBuildVersion implements JbTask {
+    static final String INPUT = "../src/main/template/jbuild/Version.java";
+    static final String OUTPUT = "../src/main/java/jbuild/Version.java";
+
+    @Override
+    public void run(String... args) throws IOException {
+        var dir = new File(OUTPUT).getParentFile();
+        if (dir == null || (!dir.isDirectory() && !dir.mkdirs())) {
+            throw new JBuildException("Cannot create output directory for " + OUTPUT, IO_WRITE);
+        }
+        writeString(Paths.get(OUTPUT),
+                readAllLines(Paths.get(INPUT))
+                        .stream().map((line) -> line.replace("%VERSION%", "1.0"))
+                        .collect(Collectors.joining(System.getProperty("line.separator"))));
+    }
+}
---
+++ src/main/template/jbuild/Version.java
@@ -0,0 +1,5 @@
+package jbuild;
+
+public final class Version {
+    public static final String VALUE = "%VERSION%";
+}
