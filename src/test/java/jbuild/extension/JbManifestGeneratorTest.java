f294eaa837ef1a6b2adc0b7b2838bbffd7e1f15e
Renato Athaydes <renato@athaydes.com>
2023-11-17 18:28:16 +0100

    More tests for jbManifest generator.

--- src/test/java/jbuild/extension/JbManifestGeneratorTest.java
+++ src/test/java/jbuild/extension/JbManifestGeneratorTest.java
@@ -10,6 +10,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JbManifestGeneratorTest {
 
     @Test
+    void canGenerateForTypicalJbTaskInfo() throws IOException {
+        var classFile = TestHelper.compileJavaClassFile("Main", "" +
+                "import jbuild.api.*;\n" +
+                "@JbTaskInfo(name = \"my-task\", \n" +
+                "  description = \"a important task.\"," +
+                "  phase = @TaskPhase(name = \"setup\"),\n" +
+                "  dependents = \"depend-on-me\",\n" +
+                "  dependsOn = {\"i-depend-on\", \"another-dep\"}\n" +
+                ")\n" +
+                "public class Main {\n" +
+                " public Main() {\n" +
+                " }\n" +
+                "}", TestHelper.ClassPathOption.Option.INHERIT);
+        var generator = new JbManifestGenerator(TestHelper.createLog(false).getKey());
+        var builder = new StringBuilder();
+
+        generator.createEntryForExtension(classFile, builder);
+
+        assertThat(builder.toString()).isEqualTo("  \"my-task\":\n" +
+                "    class-name: Main\n" +
+                "    description: a important task.\n" +
+                "    phase:\n" +
+                "      \"setup\": -1\n" +
+                "    depends-on:\n" +
+                "      - \"i-depend-on\"\n" +
+                "      - \"another-dep\"\n" +
+                "    dependents:\n" +
+                "      - \"depend-on-me\"\n" +
+                "    config-constructors:\n" +
+                "      - {}\n");
+    }
+
+    @Test
     void canGenerateSimpleConstructor() throws IOException {
         var classFile = TestHelper.compileJavaClassFile("Main", "" +
                 "import jbuild.api.*;\n" +
@@ -54,4 +87,33 @@ public class JbManifestGeneratorTest {
                 "        \"more\": \"ARRAY_OF_STRINGS\"\n" +
                 "        \"one\": \"FLOAT\"\n");
     }
+
+    @Test
+    void canGenerateMultipleConstructors() throws IOException {
+        var classFile = TestHelper.compileJavaClassFile("foo.bar.MyExtension", "" +
+                "package foo.bar;\n" +
+                "import jbuild.api.*;\n" +
+                "import java.util.List;\n" +
+                "@JbTaskInfo(name = \"my-task\")\n" +
+                "public class MyExtension {\n" +
+                " public MyExtension(boolean b, int c) {}" +
+                " public MyExtension(List<String> s, String... more) {}" +
+                " public MyExtension() {\n" +
+                " }\n" +
+                "}", TestHelper.ClassPathOption.Option.INHERIT);
+        var generator = new JbManifestGenerator(TestHelper.createLog(false).getKey());
+        var builder = new StringBuilder();
+
+        generator.createEntryForExtension(classFile, builder);
+
+        assertThat(builder.toString()).isEqualTo("  \"my-task\":\n" +
+                "    class-name: foo.bar.MyExtension\n" +
+                "    config-constructors:\n" +
+                "      - \"b\": \"BOOLEAN\"\n" +
+                "        \"c\": \"INT\"\n" +
+                "      - \"s\": \"LIST_OF_STRINGS\"\n" +
+                "        \"more\": \"ARRAY_OF_STRINGS\"\n" +
+                "      - {}\n");
+    }
+
 }
