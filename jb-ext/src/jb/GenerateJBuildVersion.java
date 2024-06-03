8253212aa9efe4c28a641b05e3d1ca5aa6366983
Renato Athaydes <renato@athaydes.com>
2024-06-03 22:17:01 +0200

    Fixed GenerateJBuildVersion ins/outs.

--- jb-ext/src/jb/GenerateJBuildVersion.java
+++ jb-ext/src/jb/GenerateJBuildVersion.java
@@ -19,8 +19,8 @@ import static jbuild.api.JBuildException.ErrorCause.IO_WRITE;
         description = "Generates a Version file so JBuild can report its own version.",
         phase = @TaskPhase(name = "codegen", index = 200))
 public class GenerateJBuildVersion implements JbTask {
-    static final String INPUT = "../src/main/template/jbuild/Version.java";
-    static final String OUTPUT = "../src/main/java/jbuild/Version.java";
+    static final String INPUT = "src/main/template/jbuild/Version.java";
+    static final String OUTPUT = "src/main/java/jbuild/Version.java";
 
     @Override
     public List<String> inputs() {
