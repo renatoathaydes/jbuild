b5d9a6772f603a2c7da404f5611cee8b52d14c69
renatoathaydes <renato@athaydes.com>
2026-04-11 19:42:33 +0200

    GroovydocToolHelper: support Groovy 4.

--- jbuild-groovy/src/jbuild/groovy/GroovydocToolHelper.java
+++ jbuild-groovy/src/jbuild/groovy/GroovydocToolHelper.java
@@ -1,10 +1,13 @@
 package jbuild.groovy;
 
+import groovy.lang.GroovySystem;
 import org.codehaus.groovy.tools.groovydoc.ClasspathResourceManager;
 import org.codehaus.groovy.tools.groovydoc.FileOutputTool;
 import org.codehaus.groovy.tools.groovydoc.GroovyDocTool;
+import org.codehaus.groovy.tools.groovydoc.ResourceManager;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 
@@ -23,9 +26,39 @@ public final class GroovydocToolHelper implements Callable<Void> {
 
     @Override
     public Void call() throws Exception {
+        var tool = createGroovyDocTool();
+
+        tool.add(args.sourceFiles);
+
+        var output = new FileOutputTool();
+        tool.renderToOutput(output, args.outputDir);
+
+        return null;
+    }
+
+    /**
+     * This class is compiled with Groovy 5. To support Groovy 4, we need to be able to instantiate
+     * {@link GroovyDocTool} with the old constructor that did not have the {@code javaVersion} parameter
+     * (the second last String parameter of the constructor).
+     *
+     * @return the groovyDoc tool.
+     */
+    private GroovyDocTool createGroovyDocTool() throws Exception {
         var resourceManager = new ClasspathResourceManager();
 
-        var tool = new GroovyDocTool(
+        int groovyVersion;
+        try {
+            groovyVersion = Integer.parseInt(GroovySystem.getShortVersion().substring(0, 1));
+        } catch (NumberFormatException e) {
+            // do not fail because of this!
+            groovyVersion = 5;
+        }
+
+        if (groovyVersion < 5) {
+            return createGroovyDocToolForGroovy4(resourceManager);
+        }
+
+        return new GroovyDocTool(
                 resourceManager,
                 args.sourceDirs,
                 args.docTemplates,
@@ -35,12 +68,18 @@ public final class GroovydocToolHelper implements Callable<Void> {
                 null,
                 new Properties()
         );
+    }
 
-        tool.add(args.sourceFiles);
-
-        var output = new FileOutputTool();
-        tool.renderToOutput(output, args.outputDir);
-
-        return null;
+    @SuppressWarnings("JavaReflectionMemberAccess")
+    private GroovyDocTool createGroovyDocToolForGroovy4(ResourceManager resourceManager) throws Exception {
+        return GroovyDocTool.class.getConstructor(ResourceManager.class,
+                        String[].class, String[].class, String[].class, String[].class, List.class, Properties.class)
+                .newInstance(resourceManager,
+                        args.sourceDirs,
+                        args.docTemplates,
+                        args.packageTemplates,
+                        args.classTemplates,
+                        Collections.emptyList(),
+                        new Properties());
     }
 }
